af2be3b17b1c31f2e54dd3e10a16eecbb71972f0
Alexandre Alves <122180782+alexandre020285@users.noreply.github.com>
2024-03-27 21:08:12 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+*** Exercício com API 
+
+**Projeto - API com Rick and Morty
+
+esse projeto foi o 1º contato em larga escala com API, procurei no youtube dicas; pois algumas coisas não sabia; como técnicas etc;
+esse projeto busca procurar ficha dos personagens apenas informando o número
+Irei me aprofundar em mais projetos desse porte, e cada vez mais aprimorar o uso de APIS
+
+OBS. não entrei tanto na estilização do html; pois foquei totalmente no desenvolvimento do API
+
+
+* Tecnologias usadas
+HTML - Javascript - css
+
+* Didiculdades encontradas (reccori a ajuda dos veteranos rsrs)
+1 - em acesar objetos dentro de outro objetos
+2 - a manipulação e transformação dos dados recolhidos, e coloca-los como informação na tela sem ter que acrescentar direto no html
+3 - o uso correto e manipulação dos dados JSON 
---
+++ index.js
@@ -0,0 +1,70 @@
+const characterId = document.getElementById('characterId');
+const btnGo = document.getElementById('btn-go');
+const btnReset = document.getElementById('btn-reset');
+const content = document.getElementById('content');
+const conteinerResult = document.getElementById('result-style');
+const image = document.getElementById('img');
+
+const fetchApi = (value) => {
+    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            return data;
+        });
+
+    return result;
+}
+
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode'];
+const newKeys = {
+    name: 'Nome',
+    status: 'Status',
+    species: 'Espécie',
+    gender: 'Gênero',
+    origin: 'Planeta de origem',
+    episode: 'Episódios',
+}
+
+const buildResult = (result) => {
+    return keys.map((key) => document.getElementById(key))
+        .map((elem) => {
+            if (elem.checked === true && (Array.isArray(result[elem.name])) === true) {
+                const arrayResult = result[elem.name].join('\r\n');
+                // console.log(arrayResult);
+                const newElem = document.createElement('p');
+                newElem.innerHTML = `${newKeys[elem.name]}: ${arrayResult}`;
+                content.appendChild(newElem);
+            } else if (elem.checked === true && (elem.name === 'origin')) {
+                const newElem = document.createElement('p');
+                newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name].name}`;
+                content.appendChild(newElem);
+            } else if (elem.checked === true && typeof (result[elem.name]) !== 'object') {
+                const newElem = document.createElement('p');
+                newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name]}`;
+                content.appendChild(newElem);
+            }
+        });
+}
+
+btnGo.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    if (characterId.value === '') {
+        return content.innerHTML = 'É necessário informar o Nº do personagem.🤔';
+    }
+
+    const result = await fetchApi(characterId.value);
+    if (content.firstChild === null) {
+        conteinerResult.className = 'result-style';
+        image.src = `${result.image}`;
+        buildResult(result);
+    } else {
+        content.innerHTML = '';
+        conteinerResult.className = 'result-style';
+        image.src = `${result.image}`;
+        buildResult(result);
+    }
+});
+
+btnReset.addEventListener('click', () => location.reload());
